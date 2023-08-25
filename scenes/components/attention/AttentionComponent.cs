6ff64ad231815d6004870b67bfd6c3db8252fcb8
Kirby Johnston <johnstk91@gmail.com>
2023-08-24 23:18:45 -0400

    create attention component to handle focus

---
+++ scenes/components/attention/AttentionComponent.cs
@@ -0,0 +1,43 @@
+using Godot;
+
+[GlobalClass]
+public partial class AttentionComponent : Node
+{
+	[Signal] public delegate void FocusChangedEventHandler(Node3D newFocus, Node3D previousFocus);
+	[Signal] public delegate void FocusGainedEventHandler(Node3D newFocus);
+	[Signal] public delegate void FocusLostEventHandler();
+
+	public Node3D CurrentFocus { get; private set; } = null;
+
+	public bool SetFocus(Node3D newFocus)
+	{
+		// no need to set if they're already the same
+		if (CurrentFocus == newFocus)
+		{
+			return false;
+		}
+
+		// if no focus and they're not equal, newFocus isn't null, so emit this
+		if (CurrentFocus == null)
+		{
+			EmitSignal(SignalName.FocusGained, newFocus);
+		}
+
+		EmitSignal(SignalName.FocusChanged, newFocus, CurrentFocus);
+		CurrentFocus = newFocus;
+		return true;
+	}
+
+	public bool LoseFocus()
+	{
+		if (CurrentFocus == null)
+		{
+			return false;
+		}
+
+		// use SetFocus to trigger changed event handler
+		SetFocus(null);
+		EmitSignal(SignalName.FocusLost);
+		return true;
+	}
+}
